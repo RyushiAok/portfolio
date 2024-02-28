d63cfbc66c2bcfc2edf3913baddb91e3886a2098
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2024-02-29 03:46:56 +0900

    feat(client): vite-cloudflare

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -1,32 +1,35 @@
-import { Theme } from '@radix-ui/themes'
-import '@radix-ui/themes/styles.css'
-import type { LinksFunction } from '@remix-run/cloudflare'
-import { cssBundleHref } from '@remix-run/css-bundle'
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
-import { Header } from '~/ui/header'
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import {
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from "@remix-run/react";
+import { Header } from "~/ui/header";
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
-]
+export function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="ja">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Theme appearance="dark">
+					<Header />
+					{children}
+				</Theme>
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
+}
 
 export default function App() {
-  return (
-    <html lang="ja" suppressHydrationWarning>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Theme appearance="dark">
-          <Header />
-          <Outlet />
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </Theme>
-      </body>
-    </html>
-  )
+	return <Outlet />;
 }
--- client/app/ui/header.tsx
+++ client/app/ui/header.tsx
@@ -1,44 +1,43 @@
-import { GitHubLogoIcon } from '@radix-ui/react-icons'
-import { Avatar, Container, Flex } from '@radix-ui/themes'
-import { Heading } from 'node_modules/@radix-ui/themes/dist/cjs'
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { Avatar, Container, Flex, Heading } from "@radix-ui/themes";
 export const Header = () => (
-  <Container
-    size={'2'}
-    position="fixed"
-    style={{
-      zIndex: 100,
-      padding: '0.5rem 0',
-      opacity: 0.985,
-      backdropFilter: 'blur(0.25rem)',
-      top: 0,
-      left: 0,
-      right: 0,
-      overscrollBehaviorY: 'contain',
-    }}
-  >
-    <Flex justify="between">
-      <Flex gap="4" align="center" pl="2">
-        <Avatar
-          src="https://avatars.githubusercontent.com/u/55625375?v=4"
-          fallback="A"
-          size={'2'}
-        />
-        <Heading size="3">RyushiAok.dev</Heading>
-      </Flex>
-      <Flex gap="4" pr="2" align="center">
-        <a
-          href="https://github.com/RyushiAok"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            display: 'flex',
-            textDecoration: 'none',
-            color: 'white',
-          }}
-        >
-          <GitHubLogoIcon width="1em" height="1em" />
-        </a>
-      </Flex>
-    </Flex>
-  </Container>
-)
+	<Container
+		size={"2"}
+		position="fixed"
+		style={{
+			zIndex: 100,
+			padding: "0.5rem 0",
+			opacity: 0.985,
+			backdropFilter: "blur(0.25rem)",
+			top: 0,
+			left: 0,
+			right: 0,
+			overscrollBehaviorY: "contain",
+		}}
+	>
+		<Flex justify="between">
+			<Flex gap="4" align="center" pl="2">
+				<Avatar
+					src="https://avatars.githubusercontent.com/u/55625375?v=4"
+					fallback="A"
+					size={"2"}
+				/>
+				<Heading size="3">RyushiAok.dev</Heading>
+			</Flex>
+			<Flex gap="4" pr="2" align="center">
+				<a
+					href="https://github.com/RyushiAok"
+					target="_blank"
+					rel="noopener noreferrer"
+					style={{
+						display: "flex",
+						textDecoration: "none",
+						color: "white",
+					}}
+				>
+					<GitHubLogoIcon width="1.25em" height="1.25em" />
+				</a>
+			</Flex>
+		</Flex>
+	</Container>
+);
